e87fe3e549c2d6e3006f84c548894bb3b5de9be0
Jorge Rivera <jorge.rivera@ssde.com.mx>
2023-09-29 23:18:31 -0600

    SSDE-02. Added icons for contacts. Added welcome page text. Added Contacts page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ssde-web';
+  title = 'SSDE';
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+interface Contacto {
+  imagen: string;
+  etiqueta: string;
+  texto: string[];
+  color: string;
+}
+
+const contactos: Contacto[] = [
+  {
+    imagen: 'cellphone', etiqueta: 'Teléfono', texto: ['+5233 2773 1504', '+5233 2226 1517'], color: 'font-purple'
+  },
+  {
+    imagen: 'envelope', etiqueta: 'Correo', texto: ['jorge.rivera@ssde.com.mx', 'contacto@ssde.com.mx'], color: 'font-blue'
+  },
+  {
+    imagen: 'message', etiqueta: 'Mesaje de texto', texto: ['+5233 2773 1504', '+5233 2226 1517'], color: 'font-red'
+  },
+  {
+    imagen: 'whatsapp', etiqueta: 'Whatsapp', texto: ['+5233 2773 1504', '+5233 2226 1517'], color: 'font-green'
+  }
+]
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss','../../app.component.scss']
 })
 export class ContactComponent {
-
+  itemList: Contacto[] = contactos;
 }
