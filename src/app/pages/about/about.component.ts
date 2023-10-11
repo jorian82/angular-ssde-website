01a2afdf9e5bb6fe8b1dec3404ce31982b4486f7
Jorge Rivera <jorge.rivera@ssde.com.mx>
2023-10-10 21:01:11 -0600

    SW-04. Added about page, Changed footer to include svg icons

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss','../../app.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
+  experience: number = 0;
+  seniority: number = 0;
 
+  constructor() {
+  }
+
+  ngOnInit() {
+    let current = new Date();
+    const start = 2004;
+    const fullstack = 2012;
+
+    this.experience = current.getUTCFullYear()-start;
+    this.seniority = current.getUTCFullYear()-fullstack;
+
+
+  }
 }
